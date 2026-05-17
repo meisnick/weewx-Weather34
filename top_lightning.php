49766b5091379225625dcac9f341789fb55f3a3c
meisnick <meisnickbuck@gmail.com>
2026-05-17 15:14:35 -0500

    lightning: combine modules — enrich top_lightning with distance(mph), month/year totals, solar W/m², popup link on Last 3 Hrs, retire lightning34 from last module slot

--- top_lightning.php
+++ top_lightning.php
@@ -1,40 +1,51 @@
 <?php
-  include('w34CombinedData.php');
-  include('settings.php');
-  //include('shared.php');
-  date_default_timezone_set($TZ);
-  header('Content-type: text/html; charset=utf-8');
-  error_reporting(0);
+include('w34CombinedData.php');
+include('settings.php');
+date_default_timezone_set($TZ);
+header('Content-type: text/html; charset=utf-8');
+error_reporting(0);
 ?>
 <body>
-  <div class="wfstrike">
-    <?php
-      //weather34 lightning
-      echo "<wfstriketoday>".$lightning['strike_count_3hr']; ?>
-    </wfstriketoday>
-  </div>
-  <div class="minwordl">Strikes</div></div>
-  <div class="mintimedatex"><value>&nbsp;Last 3 Hrs<value></div>
-  <div class='wflaststrike'>
-  <?php
-    //weather34 weather34 last detect
-    if ($lightning['last_time']>=1) {
-      echo "<spanfeelstitle>Last Strike: <orange> ".date("j M Y", $lightning['last_time'])." </orange> ";}?><br />
-  <?php
-    if ($windunit == 'mph'){
-      echo "<spanfeelstitle>Last Distance At:<orange> " .number_format($lightning['light_last_distance']*0.621371,1). "  </orange>miles";
-    }else{
-      echo "<spanfeelstitle>Last Distance At:<orange> " .$lightning['light_last_distance']. "  </orange>km";
-    }
-  ?><br />
-  <?php
-    //weather34 weather34 last detect
-    echo "<spanfeelstitle>All-time Strike Total: <orange> ".$lightning['strike_count']." </orange> ";?><br>
+<?php
+// Update time — sits top-right of the box title via absolute positioning
+$isOffline = file_exists($livedata) && time() - filemtime($livedata) > 300;
+echo '<span style="position:absolute;top:2px;right:5px;font:0.6em arial,helvetica;color:silver;line-height:1;white-space:nowrap;">';
+if ($isOffline) echo '<offline>Offline</offline>';
+else echo $online . ' ' . $weather["time"];
+echo '</span>';
+?>
+
+<div class="wfstrike">
+  <?php echo "<wfstriketoday>" . $lightning['strike_count_3hr']; ?>
+</wfstriketoday>
 </div>
+<div class="minwordl">Strikes</div></div>
+<div class="mintimedatex"><value>&nbsp;<a href="pop_lightningalmanac.php" data-lity style="color:inherit;text-decoration:none;">Last 3 Hrs</a></value></div>
+
+<div class='wflaststrike'>
+<?php
+if ($lightning['last_time'] >= 1) {
+    echo "<spanfeelstitle>Last Strike: <orange> " . date("j M Y", $lightning['last_time']) . " </orange></spanfeelstitle><br />";
+}
+if ($windunit == 'mph') {
+    echo "<spanfeelstitle>Distance: <orange> " . number_format($lightning['light_last_distance'] * 0.621371, 1) . " </orange>mi</spanfeelstitle>";
+} else {
+    echo "<spanfeelstitle>Distance: <orange> " . $lightning['light_last_distance'] . " </orange>km</spanfeelstitle>";
+}
+echo "<br />";
+echo "<spanfeelstitle>Month: <orange> " . str_replace(",", "", $weather["lightningmonth"]) . " </orange>";
+echo "&nbsp; Year: <orange> " . str_replace(",", "", $weather["lightningyear"]) . " </orange>";
+echo "&nbsp; All-time: <orange> " . $lightning['strike_count'] . " </orange></spanfeelstitle><br>";
+if (!is_null($weather["solar"])) {
+    echo "<spanfeelstitle>Solar: <orange> " . $weather["solar"] . " </orange>W/m&sup2;</spanfeelstitle>";
+}
+?>
+</div>
+
 <div class="lightningicon">
 <?php
-  // display an icon when strike(s) are detected
-  if ($lightning['strike_count_3hr'] > 0){
+if ($lightning['strike_count_3hr'] > 0) {
     echo '<img src="img/lightningalert.svg" width="20" height="20" align="right"/>';
-  }?>
+}
+?>
 </div>

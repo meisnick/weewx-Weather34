41b5eacbdaa5a16b6ac326fef3da5170d7cef179
WeatherPi <pi@weatherpi.local>
2026-05-24 02:12:01 -0500

    Re-architect Air Quality Popup into Tabbed Layout

--- aqipopup.php
+++ aqipopup.php
@@ -7,115 +7,134 @@ $forecastime = filemtime ('jsondata/uk.txt');
 <link href="css/popup.<?php echo $theme; ?>.css?version=<?php echo filemtime('css/popup.' . $theme . '.css'); ?>" rel="stylesheet prefetch">
 
 <body>
-<div class="weather34darkbrowser" style="color:<?php echo $text1 ?>;" url="The World AQI Indices are based on the US EPA (United States Environmental Protection Agency) Scale, latest 24 hour running mean for the current day."></div>  
-</br> 
-
-<table class="demo">
- 
-  
-  <tbody>
-	<tr>
-        <th style="color:<?php echo $text1 ?>;" id="CELL1">WORLD (US EPA) AQI PM<sub>2.5</sub></th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL2">Good</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL3">Moderate</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL4">Unhealthy for Sensitive Groups</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL5">Unhealthy</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL6">Very Unhealthy</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL7">Hazardous</th>
-		
-		
-	</tr>
-	
-	<tr>
-		<td style="color:<?php echo $text1 ?>;" id="CELL1"><b>Range<b></td>
-		<td style="color:black;" id="CELL3">0-50</td>
-		<td style="color:black;" id="CELL6">51-100</td>
-		<td style="color:black;" id="CELL7">101-150</td>
-		<td id="CELL9">151-200</td>
-		<td id="CELL11">201-300</td>
-		<td id="CELL10">300</td>
-		
-	</tr>
-	<tr> 	 	 	 	 	 	 	 	 	
-		<td style="color:<?php echo $text1 ?>;" id="CELL1"><b>µg/m³<b></td>
-		<td style="color:black;" id="CELL3">0-12</td>
-		<td style="color:black;" id="CELL6">12-35</td>
-		<td style="color:black;" id="CELL7">35-55</td>
-		<td id="CELL9">55-150</td>
-		<td id="CELL11">150-250</td>
-		<td id="CELL10">250</td>
-		
-	</tr>
-	</tbody>
-</table>
-
-<table class="demo">
-  <div class="weather34darkbrowser" style="color:<?php echo $text1 ?>;" url="The Defra (UK Government Department for Environment and Rural Affairs) DAQI Indices are based on the daily mean concentration for historical data, latest 24 hour running mean for the current day."></div>  
-</br>
-   
-  	
-	<thead>
-	<tr>
-        <th style="color:<?php echo $text1 ?>;" id="CELL1">UK DAQI</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL2">Low</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL3">Low</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL4">Low</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL5">Moderate</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL6">Moderate</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL7">Moderate</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL8">High</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL9">High</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL10">High</th>
-		<th style="color:<?php echo $text1 ?>;" id="CELL11">Very High</th>
-	</tr>
-	</thead>
-	<tbody>
-	<tr>
-		<td style="color:<?php echo $text1 ?>;" id="CELL1"><b>Band<b></td>
-		<td style="color:black;" id="CELL2">1</td>
-		<td style="color:black;" id="CELL3">2</td>
-		<td style="color:black;" id="CELL4">3</td>
-		<td style="color:black;" id="CELL5">4</td>
-		<td style="color:black;" id="CELL6">5</td>
-		<td style="color:black;" id="CELL7">6</td>
-		<td style="color:black;" id="CELL8">7</td>
-		<td id="CELL9">8</td>
-		<td id="CELL10">9</td>
-		<td id="CELL11">10</td>
-	</tr>
-	<tr>
-		<td style="color:<?php echo $text1 ?>;" id="CELL1"><b>PM<sub>2.5 </sub>µg/m³<b></td>
-		<td style="color:black;" id="CELL2">0-11</td>
-		<td style="color:black;" id="CELL3">12-23</td>
-		<td style="color:black;" id="CELL4">24-35</td>
-		<td style="color:black;" id="CELL5">36-41</td>
-		<td style="color:black;" id="CELL6">42-47</td>
-		<td style="color:black;" id="CELL7">48-53</td>
-		<td style="color:black;" id="CELL8">54-58</td>
-		<td id="CELL9">59-64</td>
-		<td id="CELL10">65-70</td>
-		<td id="CELL11">71 or more</td>
-	</tr>
-    <tr> 	 	 	 	 	 	 	 	 	
-		<td style="color:<?php echo $text1 ?>;" id="CELL1"><b>PM<sub>10 </sub>µg/m³<b></td>
-		<td style="color:black;" id="CELL2">0-16</td>
-		<td style="color:black;" id="CELL3">17-33</td>
-		<td style="color:black;" id="CELL4">34-50</td>
-		<td style="color:black;" id="CELL5">51-58</td>
-		<td style="color:black;" id="CELL6">59-66</td>
-		<td style="color:black;" id="CELL7">67-75</td>
-		<td style="color:black;" id="CELL8">76-83</td>
-		<td id="CELL9">84-91</td>
-		<td id="CELL10">92-100</td>
-		<td id="CELL11">101 or more</td>
-	</tr>      
-	</tbody>
-</table>
-
-<div class="weather34darkbrowser" style="color:<?php echo $text1 ?>;" url="Historical Air Quality Trends (Weekly PM2.5 / PM10)"></div>
-<br>
-<div class="chart-container">
-  <iframe src="w34highcharts/<?php echo $theme; ?>-charts.html?chart='airqualityplot'&span='weekly'&temp='<?php echo $weather['temp_units'];?>'&pressure='<?php echo $weather['barometer_units'];?>'&wind='<?php echo $weather['wind_units'];?>'&rain='<?php echo $weather['rain_units'];?>'" frameborder="0" scrolling="no" width="100%" height="100%"></iframe>
+<div class="tabs-header">
+  <button class="tablink" onclick="openTab('historical-tab', this)" id="defaultOpen">Historical Trends</button>
+  <button class="tablink" onclick="openTab('help-tab', this)">AQI Measurement Guide</button>
 </div>
+
+<!-- TAB 1: HISTORICAL TRENDS HIGHCHART -->
+<div id="historical-tab" class="tabcontent">
+  <div class="weather34darkbrowser" style="color:<?php echo $text1 ?>;" url="Historical Air Quality Trends (Weekly PM2.5 / PM10)"></div>
+  <br>
+  <div class="chart-container">
+    <iframe src="w34highcharts/<?php echo $theme; ?>-charts.html?chart='airqualityplot'&span='weekly'&temp='<?php echo $weather['temp_units'];?>'&pressure='<?php echo $weather['barometer_units'];?>'&wind='<?php echo $weather['wind_units'];?>'&rain='<?php echo $weather['rain_units'];?>'" frameborder="0" scrolling="no" width="100%" height="100%"></iframe>
+  </div>
+</div>
+
+<!-- TAB 2: MEASUREMENT GUIDE TABLES -->
+<div id="help-tab" class="tabcontent">
+  <div class="weather34darkbrowser" style="color:<?php echo $text1 ?>;" url="The World AQI Indices are based on the US EPA (United States Environmental Protection Agency) Scale, latest 24 hour running mean for the current day."></div>  
+  <br> 
+
+  <table class="demo">
+    <tbody>
+      <tr>
+        <th style="color:<?php echo $text1 ?>;" id="CELL1">WORLD (US EPA) AQI PM<sub>2.5</sub></th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL2">Good</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL3">Moderate</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL4">Unhealthy for Sensitive Groups</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL5">Unhealthy</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL6">Very Unhealthy</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL7">Hazardous</th>
+      </tr>
+      <tr>
+        <td style="color:<?php echo $text1 ?>;" id="CELL1"><b>Range<b></td>
+        <td style="color:black;" id="CELL3">0-50</td>
+        <td style="color:black;" id="CELL6">51-100</td>
+        <td style="color:black;" id="CELL7">101-150</td>
+        <td id="CELL9">151-200</td>
+        <td id="CELL11">201-300</td>
+        <td id="CELL10">300</td>
+      </tr>
+      <tr> 	 	 	 	 	 	 	 	 	
+        <td style="color:<?php echo $text1 ?>;" id="CELL1"><b>µg/m³<b></td>
+        <td style="color:black;" id="CELL3">0-12</td>
+        <td style="color:black;" id="CELL6">12-35</td>
+        <td style="color:black;" id="CELL7">35-55</td>
+        <td id="CELL9">55-150</td>
+        <td id="CELL11">150-250</td>
+        <td id="CELL10">250</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <div class="weather34darkbrowser" style="color:<?php echo $text1 ?>;" url="The Defra (UK Government Department for Environment and Rural Affairs) DAQI Indices are based on the daily mean concentration for historical data, latest 24 hour running mean for the current day."></div>  
+  <br>
+
+  <table class="demo">
+    <thead>
+      <tr>
+        <th style="color:<?php echo $text1 ?>;" id="CELL1">UK DAQI</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL2">Low</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL3">Low</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL4">Low</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL5">Moderate</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL6">Moderate</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL7">Moderate</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL8">High</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL9">High</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL10">High</th>
+        <th style="color:<?php echo $text1 ?>;" id="CELL11">Very High</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td style="color:<?php echo $text1 ?>;" id="CELL1"><b>Band<b></td>
+        <td style="color:black;" id="CELL2">1</td>
+        <td style="color:black;" id="CELL3">2</td>
+        <td style="color:black;" id="CELL4">3</td>
+        <td style="color:black;" id="CELL5">4</td>
+        <td style="color:black;" id="CELL6">5</td>
+        <td style="color:black;" id="CELL7">6</td>
+        <td style="color:black;" id="CELL8">7</td>
+        <td id="CELL9">8</td>
+        <td id="CELL10">9</td>
+        <td id="CELL11">10</td>
+      </tr>
+      <tr>
+        <td style="color:<?php echo $text1 ?>;" id="CELL1"><b>PM<sub>2.5 </sub>µg/m³<b></td>
+        <td style="color:black;" id="CELL2">0-11</td>
+        <td style="color:black;" id="CELL3">12-23</td>
+        <td style="color:black;" id="CELL4">24-35</td>
+        <td style="color:black;" id="CELL5">36-41</td>
+        <td style="color:black;" id="CELL6">42-47</td>
+        <td style="color:black;" id="CELL7">48-53</td>
+        <td style="color:black;" id="CELL8">54-58</td>
+        <td id="CELL9">59-64</td>
+        <td id="CELL10">65-70</td>
+        <td id="CELL11">71 or more</td>
+      </tr>
+      <tr> 	 	 	 	 	 	 	 	 	
+        <td style="color:<?php echo $text1 ?>;" id="CELL1"><b>PM<sub>10 </sub>µg/m³<b></td>
+        <td style="color:black;" id="CELL2">0-16</td>
+        <td style="color:black;" id="CELL3">17-33</td>
+        <td style="color:black;" id="CELL4">34-50</td>
+        <td style="color:black;" id="CELL5">51-58</td>
+        <td style="color:black;" id="CELL6">59-66</td>
+        <td style="color:black;" id="CELL7">67-75</td>
+        <td style="color:black;" id="CELL8">76-83</td>
+        <td id="CELL9">84-91</td>
+        <td id="CELL10">92-100</td>
+        <td id="CELL11">101 or more</td>
+      </tr>      
+    </tbody>
+  </table>
+</div>
+
+<script>
+function openTab(tabId, btn) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+  document.getElementById(tabId).style.display = "block";
+  btn.classList.add("active");
+}
+document.getElementById("defaultOpen").click();
+</script>
 </body>
- 
